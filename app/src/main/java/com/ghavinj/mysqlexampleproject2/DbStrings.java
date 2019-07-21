5e585708b3398909a73d0c8f87fc2a81035e9015
GhavinJ <ghavinj@icloud.com>
2019-07-21 13:23:14 -0400

    Initial commit

---
+++ app/src/main/java/com/ghavinj/mysqlexampleproject2/DbStrings.java
@@ -0,0 +1,8 @@
+package com.ghavinj.mysqlexampleproject2;
+
+public class DbStrings {
+    static final String DATABASE_URL = "72.52.246.254:3306";
+    static final String DATABASE_NAME = "ittechnician_mhs";
+    static final String USERNAME = "ittechnician_drdadmin";
+    static final String PASSWORD = "Super2019$$";
+}
